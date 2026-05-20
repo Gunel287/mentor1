22cc4f894b5a3bd7cf2760f71a254d54739b2e28
Gunel287 <gunelatluxanova@gmail.com>
2026-05-20 19:24:40 +0400

    Update string.py

--- string.py
+++ string.py
@@ -33,78 +33,8 @@ string=input("ad ata adi soyad daxil et:")
 list1=my_split(string," ")
 print(list1)
 print(list1[3]," ",list1[0][0],".",list1[1][0],".",sep="")'''
-#lab6
-#1
-'''
-a="gunel yatdi"
-b=input()
-if b in a:
-    print("simvol var")
-else:
-    print("simvol yoxdur")'''
-#2
-'''str1="aabbbAAABBccCCC"
-str2=""
-for i in str1:
-    if i=="a":
-        i="b"
-    elif i=="b":
-        i="a"
-    elif i=="A":
-        i="B"
-    elif i=="B":
-        i="A"
-    str2+=i
-print(str2)'''
-#3
-'''def my_split(a,ayirici):
-    word=""
-    list1=[]
-    for i in range(len(a)):
-        if a[i]!=" ":
-            word+=a[i]
-        elif word!="":
-            list1+=[word]
-            word=""
-    if word!="":
-        list1+=[word]
-    return list1
-a=input("daxil et")
-list1=my_split(a," ")
-print(list1)
-say=0
-for i in list1:
-    if i!=1000:
-     say+=1
-print(say)'''
-#4
-def lenn(a):
-    say=0
-    for i in a:
-        if i!=1000:
-         say+=1
-    return say
-    
-def my_split(a,ayirici):
-    word=""
-    list1=[]
-    for i in range(len(a)):
-        if a[i]!=" ":
-            word+=a[i]
-        elif word!="":
-            list1+=[word]
-            word=""
-    if word!="":
-        list1+=[word]
-    return list1
-a=input("daxil et")
-list1=my_split(a," ")
-print(list1)
-maxi=0
-for i in range(len(a)):
-    if lenn(a[i])>maxi:
-        maxi=lenn(a[i])
-print(maxi)
+
+
     
 
 
